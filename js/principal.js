41cc097bf2b3825080f849e1702feef16b822948
Aline <abelomo95@gmail.com>
2021-07-20 17:09:24 +0100

    testing table

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,42 @@
 var buttonSave = document.querySelector("#button-save");
 
-var form = document.querySelector(".form-add-favorite");
-
-
-buttonSave.addEventListener("click", function(event) {
-    event.preventDefault(); 
-
-    var movieTitle = document.createElement("td");
-    movieTitle.textContent = title;
-
-    var movieUrl = document.createElement("td");
-    movieUrl.textContent = url;
-
-    var tableTr = document.createElement("tr");
-
-    tableTr.appendChild(movieTitle);
-    tableTr.appendChild(movieUrl);
-
-    var table = document.querySelector(".tbody-table-movies")
-
-    table.appendChild(tableTr);
-
-});
-
 function saveNewMovie(form) {
     var newMovie = {
         title: form.title.value,
         url: form.url.value
     }
+    return newMovie;
 }
+
+buttonSave.addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    var form = document.querySelector(".form-add-favorite");
+    var movie = saveNewMovie(form);
+
+    console.log(movie)
+
+    var tableTR = expandTable(movie);
+
+    var table = document.querySelector(".tbody-table-movies")
+
+    table.appendChild(tableTR);
+
+});
+
+function expandTable(movie) {
+
+    var movieTitle = document.createElement("td");
+    movieTitle.textContent = movie.title;
+
+    var movieUrl = document.createElement("td");
+    movieUrl.textContent = movie.url;
+
+    var tableTr = document.createElement("tr");
+
+    tableTr.appendChild(movieTitle);
+    tableTr.appendChild(movieUrl);
+}
+
 
 
